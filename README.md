f624bbc09e3c129299caa0455c09a563aea08382
Jose Pereira <jose@remotium.com>
2015-02-17 14:18:19 -0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # gulp-metascript
+
+[MetaScript](https://github.com/dcodeIO/MetaScript) filter for Gulp.
+
+## Installation
+
+```bash
+npm install --save-dev gulp-metascript
+```
+
+## Usage
+
+```js
+var metascriptPipe = require('gulp-metascript');
+...
+gulp.task('scripts', function() {
+    return gulp.src('src/**/*.js')
+        .pipe(metascriptPipe({
+            //context variables
+            SCOPE_VARIABLE: true,
+            ...
+        }))
+        .pipe(gulp.dest('dst/'));
+});
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var through = require('through2'),
+    meta = require('metascript'),
+    PluginError = require('gulp-util/lib/PluginError'),
+    pluginName = 'gulp-metascript';
+
+function createError(file, err) {
+    if (typeof err === 'string') {
+        return new PluginError(pluginName, file.path + ': ' + err, {
+            fileName: file.path,
+            showStack: false
+        });
+    }
+
+    var msg = err.message || err.msg || /* istanbul ignore next */ 'unspecified error';
+
+    return new PluginError(pluginName, file.path + ': ' + msg, {
+        fileName: file.path,
+        lineNumber: err.line,
+        stack: err.stack,
+        showStack: false
+    });
+}
+
+module.exports = function (context) {
+        function metascript(file, encoding, callback) {
+            if (file.isNull()) {
+                return callback(null, file);
+            }
+
+            if (file.isStream()) {
+                return callback(createError(file, 'Streaming not supported'));
+            }
+
+            try {
+                file.contents = new Buffer(
+                    meta.transform(file.contents, undefined, context));
+            } catch (err) {
+                return callback(createError(file, err));
+                }
+
+                return callback(null, file);
+            }
+            return through.obj(metascript);
+        };
